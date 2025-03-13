81fb954674f5e310a3ab9c26b1237ae0b518b120
mtmarctoni <marctonimas@protonmail.com>
2025-03-13 19:10:38 +0100

    first commit

---
+++ foundry/test/Lottery.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Lottery} from "../src/Lottery.sol";
+
+contract CounterTest is Test {
+
+    function testInit() public pure {
+        assert(1 ==1);
+    }
+
+}
